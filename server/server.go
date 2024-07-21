d9d274ed1cc0106ee8e5c3c262ce0f1d2a32f764
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2024-07-20 22:12:07 -0300

    Test with mux

--- server/server.go
+++ server/server.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
 
-type CurrencyExchange struct {
+type Exchange struct {
 	Code       string `json:"code"`
 	Codein     string `json:"codein"`
 	Name       string `json:"name"`
@@ -19,16 +23,46 @@ type CurrencyExchange struct {
 }
 
 type ExchangeData struct {
-	USDBRL CurrencyExchange `json:"USDBRL"`
+	USDBRL Exchange `json:"USDBRL"`
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", HomeHandler)
+	mux.HandleFunc("/cotacao", CurrencyExchange)
 
 	http.ListenAndServe(":8080", mux)
 }
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
+func CurrencyExchange(w http.ResponseWriter, r *http.Request) {
+	req, err := http.Get("https://economia.awesomeapi.com.br/json/last/USD-BRL")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Request failed: %v\n", err)
+	}
+
+	defer req.Body.Close()
+
+	res, err := io.ReadAll(req.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to read response: %v\n", err)
+	}
+
+	var data ExchangeData
+	err = json.Unmarshal(res, &data)
+
+	fmt.Println(data)
+
+	file, err := os.Create("cotacao.txt")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create file: %v\n", err)
+	}
+
+	defer file.Close()
+
+	_, err = file.WriteString(fmt.Sprintf("Cotação do Dólar: %s\n", data.USDBRL.Bid))
+
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(data.USDBRL.Bid)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to encode JSON: %v\n", err)
+	}
 }
